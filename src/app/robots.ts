97189dc574b89375d94f363cdeb4cc9e89922ae3
Alejandro Quiroz <fquirozcompean-lab@users.noreply.github.com>
2026-05-13 07:18:51 -0600

    agregar sitemap y robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://urologiaroboticaleon.com/sitemap.xml',
+  }
+}
